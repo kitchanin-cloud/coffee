9b7a6b787688f903845179058356071314907bdc
kitchanin-cloud <kitchanin@gmail.com>
2025-09-09 14:46:11 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,8 @@
+// 咖啡豆价格历史数据
+export const priceData = [
+  { date: '2025-09-08', price: '320' },
+  { date: '2025-09-07', price: '318' },
+  { date: '2025-09-06', price: '322' },
+  { date: '2025-09-05', price: '315' },
+  { date: '2025-09-04', price: '319' },
+];
